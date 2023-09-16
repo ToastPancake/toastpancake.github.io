0e5bdcb6679e0bb31179b854921cbcf52ef1bda2
ToastPancake <72112982+ToastPancake@users.noreply.github.com>
2023-09-16 01:32:14 -0400

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -8,5 +8,13 @@ function uploadImage(){
   let imgLink = URL.createObjectURL(inputFile.files[0]);
   imageView.style.backgroundImage = `url(${imgLink})`;
   imageView.textContent = "";
-  imageView.style.border = 0
+  imageView.style.border = 0;
 }
+
+dropArea.addEventListener("dragover", function(e){
+  e.preventDefault();
+})
+dropArea.addEventListener("drop", function(e){
+  e.preventDefault();
+  inputFile.files = e.dataTransfer.files;
+})
