6cca81bad145229843f3a39ab885a36f9cf41ba5
ToastPancake <72112982+ToastPancake@users.noreply.github.com>
2023-09-15 23:55:35 -0400

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -5,5 +5,6 @@ const imageView = document.getElementById("img-view");
 inputFile.addEventListener("change", uploadImage);
 
 function uploadImage(){
-  inputFile.files[0];
+  let imgLink = URL.createObjectURL(inputFile.files[0]);
+  imageView.style.backgroundImage = 'url(${imgLink})';
 }
