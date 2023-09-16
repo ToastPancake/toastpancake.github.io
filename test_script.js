8ec5c0924f04cf2dd183bef6fcb3222646448db3
ToastPancake <72112982+ToastPancake@users.noreply.github.com>
2023-09-15 23:53:54 -0400

    Update test_script.js

--- test_script.js
+++ test_script.js
@@ -1,1 +1,9 @@
+const dropArea = document.getElementById("drop-area");
+const inputFile = document.getElementById("input-file");
+const imageView = document.getElementById("img-view");
 
+inputFile.addEventListener("change", uploadImage);
+
+function uploadImage(){
+  inputFile.files[0];
+}
