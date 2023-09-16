684d52c4175ad797aa8baf083af79711fc9e8cb1
gshao18 <144626647+gshao18@users.noreply.github.com>
2023-09-16 13:48:06 -0400

    Delete test.py

--- test.py
+++
@@ -1,8 +0,0 @@
-from splinter import Browser
-from selenium.webdriver.firefox.service import Service
-
-my_service = Service()
-browser = Browser('firefox', service=my_service)
-browser.visit('https://chat.openai.com/')
-browser.find_by_id('prompt-textarea').fill('In one number, what is the average weight of a human.')
-browser.find_by_css('.icon-sm m-1 md:m-0').click()
